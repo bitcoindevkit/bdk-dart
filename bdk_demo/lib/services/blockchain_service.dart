8d28f676c5b76bac272efcfaae9c6eb2fa153a62
Johnosezele <johnosezele@gmail.com>
2026-03-06 04:57:41 +0100

    scaffold initial architecture for bdk-dart flutter reference app

---
+++ bdk_demo/lib/services/blockchain_service.dart
@@ -0,0 +1,5 @@
+class BlockchainService {
+  // TODO: Implement Esplora/Electrum client creation.
+  // TODO: Implement sync/fullScan with fullScanCompleted logic.
+  // TODO: Implement broadcast and fee estimation.
+}
