41d21f7389cda73aa3649d012956d85128e07a43
Johnosezele <johnosezele@gmail.com>
2026-03-13 15:26:30 +0100

    Make walletRecordsProvider reactive with Notifier

--- bdk_demo/lib/providers/wallet_providers.dart
+++ bdk_demo/lib/providers/wallet_providers.dart
@@ -16,10 +16,37 @@ class ActiveWalletRecordNotifier extends Notifier<WalletRecord?> {
   void clear() => state = null;
 }
 
-final walletRecordsProvider = Provider<List<WalletRecord>>((ref) {
-  final storage = ref.watch(storageServiceProvider);
-  return storage.getWalletRecords();
-});
+final walletRecordsProvider =
+    NotifierProvider<WalletRecordsNotifier, List<WalletRecord>>(
+      WalletRecordsNotifier.new,
+    );
+
+class WalletRecordsNotifier extends Notifier<List<WalletRecord>> {
+  @override
+  List<WalletRecord> build() {
+    final storage = ref.watch(storageServiceProvider);
+    return storage.getWalletRecords();
+  }
+
+  Future<void> addWalletRecord(
+    WalletRecord record,
+    WalletSecrets secrets,
+  ) async {
+    final storage = ref.read(storageServiceProvider);
+    await storage.addWalletRecord(record, secrets);
+    state = storage.getWalletRecords();
+  }
+
+  Future<void> setFullScanCompleted(String walletId) async {
+    final storage = ref.read(storageServiceProvider);
+    await storage.setFullScanCompleted(walletId);
+    state = storage.getWalletRecords();
+  }
+
+  void refresh() {
+    state = ref.read(storageServiceProvider).getWalletRecords();
+  }
+}
 
 // TODO: Add activeWalletProvider.
 // TODO: Add balanceProvider, syncStateProvider.
