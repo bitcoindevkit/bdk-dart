5eed879868fec95e68c712bc26047cb7760d8696
Matthew <ramsden@hey.com>
2026-04-15 17:37:27 -0500

    chore: add package-root dart library export

---
+++ lib/bdk_dart.dart
@@ -0,0 +1,7 @@
+/// Package-root export for `bdk_dart`.
+///
+/// The generated UniFFI bindings live in `bdk.dart`; this file provides the
+/// conventional `package:bdk_dart/bdk_dart.dart` entrypoint expected by pub.dev.
+library;
+
+export 'bdk.dart';
