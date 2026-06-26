f483bb530befc6137d8c2eabc5751c825a21bd04
Jeremiah Jacob <jaykon4me@gmail.com>
2026-06-26 12:13:18 +0100

    test: cover independent SQLite keychain indexes

--- test/integration/wallet_creation_test.dart
+++ test/integration/wallet_creation_test.dart
@@ -281,5 +281,96 @@ void main() {
         changeDescriptor.dispose();
       }
     });
+
+    test('cover independent SQLite keychain indexes', () {
+      final tempDir = Directory.systemTemp.createTempSync(
+        'bdk_dart_wallet_creation_test3_',
+      );
+      addTearDown(() => _deleteDirectoryWithRetry(tempDir));
+
+      final dbPath = '${tempDir.path}/wallet.sqlite';
+      final descriptor = buildBip84Descriptor(Network.testnet);
+      final changeDescriptor = buildBip84ChangeDescriptor(Network.testnet);
+      Persister? persister;
+      Wallet? wallet;
+      Persister? reopenedPersister;
+      Wallet? reopenedWallet;
+
+      try {
+        final initialPersister = Persister.newSqlite(path: dbPath);
+        persister = initialPersister;
+
+        wallet = Wallet(
+          descriptor: descriptor,
+          changeDescriptor: changeDescriptor,
+          network: Network.testnet,
+          persister: initialPersister,
+          lookahead: defaultLookahead,
+        );
+
+        final ext0 = wallet.revealNextAddress(keychain: KeychainKind.external_);
+        final ext1 = wallet.revealNextAddress(keychain: KeychainKind.external_);
+        final ext2 = wallet.revealNextAddress(keychain: KeychainKind.external_);
+
+        final int0 = wallet.revealNextAddress(keychain: KeychainKind.internal);
+        final int1 = wallet.revealNextAddress(keychain: KeychainKind.internal);
+
+        expect(ext0.index, equals(0));
+        expect(ext1.index, equals(1));
+        expect(ext2.index, equals(2));
+
+        expect(int0.index, equals(0));
+        expect(int1.index, equals(1));
+
+        expect(wallet.persist(persister: initialPersister), isTrue);
+
+        wallet.dispose();
+        wallet = null;
+        persister.dispose();
+        persister = null;
+
+        reopenedPersister = Persister.newSqlite(path: dbPath);
+        reopenedWallet = Wallet.load(
+          descriptor: descriptor,
+          changeDescriptor: changeDescriptor,
+          persister: reopenedPersister,
+          lookahead: defaultLookahead,
+        );
+
+        expect(
+          reopenedWallet.nextDerivationIndex(keychain: KeychainKind.external_),
+          equals(3),
+        );
+        expect(
+          reopenedWallet.nextDerivationIndex(keychain: KeychainKind.internal),
+          equals(2),
+        );
+
+        final reopenedExt1 = reopenedWallet.peekAddress(
+          keychain: KeychainKind.external_,
+          index: 1,
+        );
+        final reopenedInt0 = reopenedWallet.peekAddress(
+          keychain: KeychainKind.internal,
+          index: 0,
+        );
+
+        expect(
+          reopenedExt1.address.scriptPubkey().toBytes(),
+          orderedEquals(ext1.address.scriptPubkey().toBytes()),
+        );
+        expect(
+          reopenedInt0.address.scriptPubkey().toBytes(),
+          orderedEquals(int0.address.scriptPubkey().toBytes()),
+        );
+      } finally {
+        reopenedWallet?.dispose();
+        reopenedPersister?.dispose();
+        wallet?.dispose();
+        persister?.dispose();
+        descriptor.dispose();
+        changeDescriptor.dispose();
+      }
+    });
   });
 }
